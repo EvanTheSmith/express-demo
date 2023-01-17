247cd09211ad53591c846804cbf9eb192ef4139e
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-17 09:53:31 -0600

    testing params and queries

--- index.js
+++ index.js
@@ -22,3 +22,8 @@ app.get('/api/courses', (request, response) => response.send([1, 2, 3]));
 // on Windows, use "set" to setup an environment variable; on Mac, use "export"
 // in this app, I used `export PORT=4000' to setup the environment variable for PORT
 // to view all environment variables: console.log(process.env);
+
+// Route Parameter : '/2018' in browser, ':year' (or any name) in backend (stored in request.params)
+// Query String Parameter : '?somethingElse=true' in browser (stored in request.query)
+
+app.get('/api/courses/:id', (request, response) => response.send(request.params));
