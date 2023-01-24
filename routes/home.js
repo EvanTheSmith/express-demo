7cdcfe2ebab1f010be6fbac872490d366065d4b8
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-24 15:34:53 -0600

    troubleshooting routes. home route fixed. courses still buggy

--- routes/home.js
+++ routes/home.js
@@ -5,4 +5,4 @@ router.get('/', (req, res) => {
     res.render('index', {title: "Express Demo App", message: "Hello World"}); // a pug template, passed with an object holding 2 variables
 });
 
-module.export = router;
+module.exports = router;
